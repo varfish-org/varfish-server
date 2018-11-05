2e55b54ddcd24866c6ab28f53e9ea3bd06daf607
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-11-05 17:42:35 +0100

    Adding sqlalchemy toolbar plugin.

--- config/settings/local.py
+++ config/settings/local.py
@@ -37,7 +37,10 @@ CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-INSTALLED_APPS += ["debug_toolbar"]
+INSTALLED_APPS += [
+    "debug_toolbar",
+    "debug_toolbar_sqlalchemy",
+]
 
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
@@ -45,6 +48,25 @@ DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
+
+DEBUG_TOOLBAR_PANELS = [
+    # Basic values
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    # Third-party app toolbar panels
+    'debug_toolbar_sqlalchemy.panel.SqlAlchemyDebugPanel',
+]
+
 
 # Celery
 # ------------------------------------------------------------------------------
