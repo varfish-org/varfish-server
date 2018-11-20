f8a4d8a17ce1205d3e1908ec31d7e1b5de16fa03
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-11-20 16:07:34 +0100

    Registering more models in Django admin.

--- variants/admin.py
+++ variants/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Case, SmallVariant, ExportFileBgJob, ExportFileJobResult, SmallVariantFlags
+from .models import (
+    Case,
+    SmallVariant,
+    ExportFileBgJob,
+    ExportFileJobResult,
+    SmallVariantFlags,
+    SmallVariantQuery,
+    SmallVariantComment,
+)
 
 # Register your models here.
 admin.site.register(Case)
@@ -8,3 +16,5 @@ admin.site.register(SmallVariant)
 admin.site.register(ExportFileBgJob)
 admin.site.register(ExportFileJobResult)
 admin.site.register(SmallVariantFlags)
+admin.site.register(SmallVariantComment)
+admin.site.register(SmallVariantQuery)
