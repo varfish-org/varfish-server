ba2eef73e89f4c5e3aaab3134059aa24d6c8bb72
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2019-03-18 18:28:15 +0100

    Allow reading of gziped db info files.

--- importer/management/helpers/tsv_reader.py
+++ importer/management/helpers/tsv_reader.py
@@ -1,10 +1,16 @@
+import gzip
+
 def tsv_reader(path):
     """Read any info file in TSV format with first line as header.
 
     :param path: Path to the info file.
     :return: Yield dict with column names as keys and the values as values
     """
-    with open(path, "r") as fh:
+    if path.endswith(".gz"):
+        inputf = gzip.open(path, "rt")
+    else:
+        inputf = open(path, "rt")
+    with inputf as fh:
         keys = next(fh).rstrip("\n").split("\t")
         for line in fh:
             yield dict(zip(keys, line.rstrip("\n").split("\t")))
