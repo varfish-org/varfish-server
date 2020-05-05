3dccf4065a5df6e87ec86fd403ee22f04a4574f2
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-05-05 11:50:46 +0200

    Removing tokens app in favor of sodar-core one.

--- tokens/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TokensConfig(AppConfig):
-    name = "tokens"
--- tokens/rules.py
+++
@@ -1,7 +0,0 @@
-"""Permissions configured using django-rules."""
-
-import rules
-
-
-# ``tplems.access`` -- Access to the tokens app.
-rules.add_perm("tokens.access", rules.is_active)
