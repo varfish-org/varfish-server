3f58fce15d7024fd6bdcdd7f5dce617f93ab077a
Oliver Stolpe <oliver.stolpe@bihealth.de>
2020-08-11 14:21:12 +0200

    Increased logging during sample variant stats computation during case import.

--- variants/models.py
+++ variants/models.py
@@ -3487,7 +3487,9 @@ class VariantImporter(VariantImporterBase):
 
         before = timezone.now()
         self.import_job.add_log_entry("Computing variant statistics...")
-        rebuild_case_variant_stats(SQLALCHEMY_ENGINE, variant_set)
+        rebuild_case_variant_stats(
+            SQLALCHEMY_ENGINE, variant_set, logger=self.import_job.add_log_entry
+        )
         elapsed = timezone.now() - before
         self.import_job.add_log_entry(
             "Finished computing variant statistics in %.2f s" % elapsed.total_seconds()
