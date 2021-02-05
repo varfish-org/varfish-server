21136284802c7a643336c991c12155cd52a71b23
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2021-02-05 17:14:07 +0100

    Fixing permissions for cases.

--- variants/rules.py
+++ variants/rules.py
@@ -11,7 +11,25 @@ rules.add_perm(
     | pr_rules.is_project_guest,
 )
 
-rules.add_perm("variants.add_case", rules.is_superuser | pr_rules.is_project_contributor)
+rules.add_perm(
+    "variants.add_case",
+    rules.is_superuser
+    | pr_rules.is_project_owner
+    | pr_rules.is_project_delegate
+    | pr_rules.is_project_contributor,
+)
 rules.add_perm("variants.delete_case", rules.is_superuser)
-rules.add_perm("variants.update_case", rules.is_superuser | pr_rules.is_project_contributor)
-rules.add_perm("variants.sync_remote", rules.is_superuser)
+rules.add_perm(
+    "variants.update_case",
+    rules.is_superuser
+    | pr_rules.is_project_owner
+    | pr_rules.is_project_delegate
+    | pr_rules.is_project_contributor,
+)
+rules.add_perm(
+    "variants.sync_remote",
+    rules.is_superuser
+    | pr_rules.is_project_owner
+    | pr_rules.is_project_delegate
+    | pr_rules.is_project_contributor,
+)
