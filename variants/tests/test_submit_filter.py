f543bafb6ee4a683c103b15398fa72450756cf2a
Meghna Ahuja Bhasin <ahujameg4@gmail.com>
2022-09-02 17:59:29 +0200

    Implemented phenotype prioritization by CADA ticket #596

--- variants/tests/test_submit_filter.py
+++ variants/tests/test_submit_filter.py
@@ -18,6 +18,7 @@ from variants.tests.factories import (
 
 from ..models import (
     CaddPathogenicityScoreCache,
+   # CadaPathogenicityScoreCache,
     MutationTasterPathogenicityScoreCache,
     ProjectCasesSmallVariantQuery,
     SmallVariantQuery,
@@ -233,7 +234,7 @@ class CaseFilterTest(TestCase):
         self.assertEqual(CaddPathogenicityScoreCache.objects.count(), 3)
 
     @patch("django.conf.settings.VARFISH_ENABLE_CADD", True)
-    @patch("django.conf.settings.VARFISH_CADD_REST_API_URL", "https://cadd.com")
+    @patch("django.conf.settings.VARFISH_CADD_REST_API_URL", "https://cadd.com") ########### Implement for PEDIA
     @Mocker()
     def test_submit_case_filter_cadd(self, mock):
         def _key_gen(s):
