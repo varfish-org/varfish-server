60bc044a8ff676df8c7e17ed8ec6e168798fcf6c
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2022-09-28 09:15:17 +0200

    Removing dependency on bootstrap-vue (#716)
    Closes: #716
    Related-Issue: #716
    Projected-Results-Impact: none

--- clinvar_export/vueapp/src/components/SubmissionSetWizardFooter.vue
+++ clinvar_export/vueapp/src/components/SubmissionSetWizardFooter.vue
@@ -1,41 +1,49 @@
 <template>
-  <b-button-group class="float-right">
-    <b-button
+  <div class="btn-group float-right">
+    <button
       ref="buttonCancel"
-      variant="secondary"
+      type="button"
+      class="btn btn-secondary"
       @click="$emit('cancel-clicked')"
     >
       <span class="iconify" data-icon="mdi:close" data-inline="false"></span>
       Cancel
-    </b-button>
-    <b-button ref="buttonSave" variant="success" @click="$emit('save-clicked')">
+    </button>
+    <button
+      ref="buttonSave"
+      type="button"
+      class="btn btn-success"
+      @click="$emit('save-clicked')"
+    >
       <span
         class="iconify"
         data-icon="mdi:check-bold"
         data-inline="false"
       ></span>
       Save
-    </b-button>
+    </button>
 
-    <b-button
+    <button
       v-if="wizardState === 'submissionSet'"
       ref="buttonVariants"
-      variant="primary"
+      type="button"
+      class="btn btn-primary"
       @click="$emit('goto-submissions-clicked')"
     >
       <i class="iconify" data-icon="mdi:arrow-right-circle"></i>
       Variants
-    </b-button>
-    <b-button
+    </button>
+    <button
       v-if="wizardState === 'submissions'"
       ref="buttonSubmission"
-      variant="primary"
+      type="button"
+      class="btn btn-primary"
       @click="$emit('goto-submission-set-clicked')"
     >
       <i class="iconify" data-icon="mdi:arrow-left-circle"></i>
       Submission
-    </b-button>
-  </b-button-group>
+    </button>
+  </div>
 </template>
 
 <script>
