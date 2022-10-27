fa827fd306cbc5321fe5b45f7236c5c4bdeb2c40
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2022-10-27 14:36:28 +0200

    Fixing vite build for production.
    No-Related-Issue: small build issue only
    Projected-Results-Impact: none

--- varfish/vueapp/vite.config.js
+++ varfish/vueapp/vite.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/static/vueapp/',
   build: {
     // generate manifest.json in outDir
     manifest: true,
