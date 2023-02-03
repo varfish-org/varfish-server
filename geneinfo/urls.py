c8191810af232d8467a83d52b705585891e620e2
Oliver Stolpe <oliver.stolpe@bih-charite.de>
2023-02-03 09:19:04 +0100

    feat: Adding presets to gene allowlist in vue filter app (#824)
    Closes: #824
    Related-Issue: #824
    Projected-Results-Impact: none

--- geneinfo/urls.py
+++ geneinfo/urls.py
@@ -8,6 +8,16 @@ api_urlpatterns = [
         regex=r"^api/lookup-gene/$", view=views_api.LookupGeneApiView.as_view(), name="lookup-gene"
     ),
     url(
+        regex=r"^api/lookup-genepanel/$",
+        view=views_api.LookupGenePanelApiView.as_view(),
+        name="lookup-genepanel",
+    ),
+    url(
+        regex=r"^api/genepanel-category/list/$",
+        view=views_api.GenePanelCategoryListApiView.as_view(),
+        name="gene-panel-category-list",
+    ),
+    url(
         regex=r"^api/gene-infos/(?P<database>[a-z]+)/(?P<geneid>[0-9a-zA-Z]+)/?$",
         view=views_api.GeneInfosApiView.as_view(),
         name="api-gene-infos",
