87f24b4f89c1d8003d0c15217bea9be20567fbcb
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-02-17 17:12:04 +0100

    feat: implement expert load/save for genotype criteria (#872) (#875)

---
+++ svs/vueapp/src/components/SvFilterFormDev.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+
+// Define the component's props.
+const props = defineProps({
+  querySettings: Object,
+})
+
+const rawFilterCriteriaRef = ref(null)
+
+const loadSettings = () => {
+  rawFilterCriteriaRef.value.value = JSON.stringify(
+    props.querySettings.genotype_criteria,
+    null,
+    2
+  )
+}
+
+const applySettings = () => {
+  props.querySettings.genotype_criteria = JSON.parse(
+    rawFilterCriteriaRef.value.value
+  )
+}
+</script>
+
+<template>
+  <div class="row">
+    <div class="col mt-2 mb-2">
+      <div class="form-group">
+        <label for="raw-filter-criteria-ref">Raw Filter Criteria</label>
+        <textarea
+          ref="rawFilterCriteriaRef"
+          id="raw-filter-criteria-ref"
+          class="form-control"
+          rows="5"
+        ></textarea>
+      </div>
+
+      <div class="mt-2">
+        <button class="btn btn-primary" @click.prevent="loadSettings()">
+          <i-mdi-download />
+          Fetch Criteria
+        </button>
+
+        <button class="btn btn-primary ml-2" @click.prevent="applySettings()">
+          <i-mdi-upload />
+          Apply Criteria
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
