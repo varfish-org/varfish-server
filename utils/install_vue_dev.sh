3d5cb9fcf2bc44663a74e1a21ec44275d68963ee
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-07-31 17:19:45 +0200

    bump node to 20

--- utils/install_vue_dev.sh
+++ utils/install_vue_dev.sh
@@ -2,5 +2,5 @@
 echo "***********************************************"
 echo "Installing Node.js"
 echo "***********************************************"
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
