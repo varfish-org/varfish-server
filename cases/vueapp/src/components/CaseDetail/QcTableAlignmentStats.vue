6f0cfa0e436ac0d72530fa9e214f3cf85f4b54ce
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-10 11:23:59 +0100

    fix: suppress BAM QC table if BAM stats missing (#1213)

--- cases/vueapp/src/components/CaseDetail/QcTableAlignmentStats.vue
+++ cases/vueapp/src/components/CaseDetail/QcTableAlignmentStats.vue
@@ -40,95 +40,100 @@ const percentDuplicates = (bamstats) => {
 </script>
 
 <template>
-  <h6 class="mt-3">Stats</h6>
+  <div v-if="bamstats">
+    <h6 class="mt-3">Stats</h6>
 
-  <table class="table table-striped table-hover">
-    <thead>
-      <tr>
-        <th style="width: 15%" class="text-center">Sample</th>
-        <th style="width: 15%" class="text-right">Pairs</th>
-        <th style="width: 15%" class="text-right">Duplicates</th>
-        <th style="width: 15%" class="text-right">&#248; Insert Size</th>
-        <th style="width: 15%" class="text-right">SD Insert Size</th>
-        <th style="width: 15%" class="text-right">Mean Target Coverage</th>
-        <th style="width: 15%" class="text-right">Target Size</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="member of caseDetailsStore.caseObj.pedigree">
-        <th>{{ displayName(member.name) }}</th>
-        <td class="text-right">
-          {{ formatLargeInt(bamStats[member.name].bamstats?.sequences / 2) }}
-        </td>
-        <td class="text-right">
-          {{ percentDuplicates(bamStats[member.name].bamstats).toFixed(1) }}%
-        </td>
-        <td class="text-right">
-          {{ bamStats[member.name].bamstats?.['insert size average'] ?? '-' }}
-        </td>
-        <td class="text-right">
-          {{
-            bamStats[member.name].bamstats?.[
-              'insert size standard deviation'
-            ] ?? '-'
-          }}
-        </td>
-        <td
-          class="text-right"
-          v-if="bamStats[member.name].summary?.['mean coverage']"
-        >
-          {{ bamStats[member.name].summary['mean coverage'] }}&nbsp;x
-        </td>
-        <td v-else class="text-right">-</td>
-        <td
-          class="text-right"
-          v-if="bamStats[member.name].summary?.['total target size']"
-        >
-          {{
-            bamStats[member.name].summary?.['total target size'] ?? '-'
-          }}&nbsp;x
-        </td>
-        <td v-else class="text-right">-</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <h6 class="mt-3">Target Coverage</h6>
-
-  <table class="table table-striped table-hover">
-    <thead>
-      <tr>
-        <th style="width: 15%" class="text-center">Sample</th>
-        <th
-          v-for="coverage of coverages"
-          style="width: 0"
-          class="text-nowrap text-right"
-        >
-          ≥ {{ coverage }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <template
-        v-if="caseDetailsStore.storeState.state === State.Active && bamStats"
-      >
-        <tr v-for="member of caseDetailsStore.caseObj.pedigree">
-          <template v-if="member.name in bamStats">
-            <th>{{ displayName(member.name) }}</th>
-            <td v-for="coverage in coverages" class="text-right">
-              {{ getMinCovTarget(member.name, coverage) }}
-            </td>
-          </template>
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th style="width: 15%" class="text-center">Sample</th>
+          <th style="width: 15%" class="text-right">Pairs</th>
+          <th style="width: 15%" class="text-right">Duplicates</th>
+          <th style="width: 15%" class="text-right">&#248; Insert Size</th>
+          <th style="width: 15%" class="text-right">SD Insert Size</th>
+          <th style="width: 15%" class="text-right">Mean Target Coverage</th>
+          <th style="width: 15%" class="text-right">Target Size</th>
         </tr>
-      </template>
-      <tr v-else>
-        <td
-          :colspan="coverages.length + 4"
-          class="text-center text-muted font-italic"
+      </thead>
+      <tbody>
+        <tr v-for="member of caseDetailsStore.caseObj.pedigree">
+          <th>{{ displayName(member.name) }}</th>
+          <td class="text-right">
+            {{ formatLargeInt(bamStats[member.name].bamstats?.sequences / 2) }}
+          </td>
+          <td class="text-right">
+            {{ percentDuplicates(bamStats[member.name].bamstats).toFixed(1) }}%
+          </td>
+          <td class="text-right">
+            {{ bamStats[member.name].bamstats?.['insert size average'] ?? '-' }}
+          </td>
+          <td class="text-right">
+            {{
+              bamStats[member.name].bamstats?.[
+                'insert size standard deviation'
+              ] ?? '-'
+            }}
+          </td>
+          <td
+            class="text-right"
+            v-if="bamStats[member.name].summary?.['mean coverage']"
+          >
+            {{ bamStats[member.name].summary['mean coverage'] }}&nbsp;x
+          </td>
+          <td v-else class="text-right">-</td>
+          <td
+            class="text-right"
+            v-if="bamStats[member.name].summary?.['total target size']"
+          >
+            {{
+              bamStats[member.name].summary?.['total target size'] ?? '-'
+            }}&nbsp;x
+          </td>
+          <td v-else class="text-right">-</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h6 class="mt-3">Target Coverage</h6>
+
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th style="width: 15%" class="text-center">Sample</th>
+          <th
+            v-for="coverage of coverages"
+            style="width: 0"
+            class="text-nowrap text-right"
+          >
+            ≥ {{ coverage }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template
+          v-if="caseDetailsStore.storeState.state === State.Active && bamStats"
         >
-          No coverage information provided.
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          <tr v-for="member of caseDetailsStore.caseObj.pedigree">
+            <template v-if="member.name in bamStats">
+              <th>{{ displayName(member.name) }}</th>
+              <td v-for="coverage in coverages" class="text-right">
+                {{ getMinCovTarget(member.name, coverage) }}
+              </td>
+            </template>
+          </tr>
+        </template>
+        <tr v-else>
+          <td
+            :colspan="coverages.length + 4"
+            class="text-center text-muted font-italic"
+          >
+            No coverage information provided.
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-else class="text-muted align-center">
+    BAM statistics not available.
+  </div>
 </template>
