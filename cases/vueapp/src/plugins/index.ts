64c99e65f17770ec0f0427dfe406867af2a983e4
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-23 10:15:22 +0100

    fix: timing issue when initializing pinia (#1374)

--- cases/vueapp/src/plugins/index.ts
+++ cases/vueapp/src/plugins/index.ts
@@ -2,10 +2,10 @@ import type { App } from 'vue'
 
 import { router } from '../router'
 import { vuetify } from './vuetify'
-import { pinia } from '../stores'
+import { pinia } from './pinia'
 import { setupBackendUrls } from './reevFrontend'
 
 export async function registerPlugins(app: App) {
   setupBackendUrls()
-  app.use(vuetify).use(router).use(pinia)
+  app.use(pinia).use(vuetify).use(router)
 }
