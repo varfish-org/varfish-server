b2f4902eef4ff726d3e16af3780cc823ebdc87e1
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-07-01 16:53:00 +0200

    wip

--- backend/clinvar_export/plugins.py
+++ backend/clinvar_export/plugins.py
@@ -8,7 +8,6 @@ class ProjectAppPlugin(ProjectAppPluginPoint):
 
     name = "clinvar_export"
     title = "REMOVED"
-    state = 2  # removed ... however this does not work, it stays active forever
     plugin_ordering = 100
     icon = "mdi:export-variant"
     entry_point_url_id = "projectroles:detail"
