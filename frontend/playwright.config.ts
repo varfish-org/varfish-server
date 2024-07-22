69f86bd9a9ef5ddd6a98d761c67a4ae0b2c9b746
Gregor Weber <mail@watwa.re>
2024-07-22 20:16:15 +0200

    gh action

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
     },
   ],
 
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run storybook',
+    url: 'http://127.0.0.1:6006',
+    reuseExistingServer: !process.env.CI,
+  },
 })
