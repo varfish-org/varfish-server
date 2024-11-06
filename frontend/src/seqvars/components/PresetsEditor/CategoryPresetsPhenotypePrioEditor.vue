e2f86c676edfcc6d5721a2349d3bda1619d04a4a
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-11-06 15:23:21 +0100

    variant and phenotype prio

--- frontend/src/seqvars/components/PresetsEditor/CategoryPresetsPhenotypePrioEditor.vue
+++ frontend/src/seqvars/components/PresetsEditor/CategoryPresetsPhenotypePrioEditor.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
+import { useQueryClient } from '@tanstack/vue-query'
 import { SeqvarsQueryPresetsPhenotypePrio } from '@varfish-org/varfish-api/lib'
-import { debounce } from 'lodash'
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { VForm } from 'vuetify/lib/components/index.mjs'
 
-import { useSeqvarsPresetsStore } from '@/seqvars/stores/presets'
+import {
+  invalidateSeqvarQueryPresetsSetVersionKeys,
+  useSeqvarQueryPresetsSetVersionRetrieveQuery,
+} from '@/seqvars/queries/seqvarQueryPresetSetVersion'
+import {
+  useSeqvarsQueryPresetsPhenotypePrioRetrieveQuery,
+  useSeqvarsQueryPresetsPhenotypePrioUpdateMutation,
+} from '@/seqvars/queries/seqvarQueryPresetsPhenotypePrio'
 import { PresetSetVersionState } from '@/seqvars/stores/presets/types'
 import { SnackbarMessage } from '@/seqvars/views/PresetSets/lib'
-
-import { CATEGORY_PRESETS_DEBOUNCE_WAIT } from './lib'
 
 /** This component's props. */
 const props = withDefaults(
   defineProps<{
+    /** UUID of the current presets set. */
+    presetSet?: string
     /** UUID of the current preset set version. */
     presetSetVersion?: string
     /** UUID of the query presets phenotype prio */
@@ -29,25 +36,44 @@ const emit = defineEmits<{
   message: [message: SnackbarMessage]
 }>()
 
-/** Store with the presets. */
-const seqvarsPresetsStore = useSeqvarsPresetsStore()
+/** The `QueryClient` for explicit invalidation.*/
+const queryClient = useQueryClient()
 
-/** The data that is to be edited by this component; component state. */
-const data = ref<SeqvarsQueryPresetsPhenotypePrio | undefined>(undefined)
+/** Presets set UUID as `ComputedRef` for queries. */
+const presetsSetUuid = computed<string | undefined>(() => {
+  return props.presetSet
+})
+/** Presets set version UUID as `ComputedRef` for queries. */
+const presetsSetVersionUuid = computed<string | undefined>(() => {
+  return props.presetSetVersion
+})
+/** PhenotypePrio presets UUID as `ComputedRef` for queries. */
+const presetsPhenotypePrioUuid = computed<string | undefined>(() => {
+  return props.phenotypePrioPresets
+})
+
+/** Query with the currently selected presets set version. */
+const presetsSetVersionRetrieveRes =
+  useSeqvarQueryPresetsSetVersionRetrieveQuery({
+    presetsSetUuid,
+    presetsSetVersionUuid,
+  })
+/** Query with the currently selected phenotypePrio presets. */
+const presetsPhenotypePrioRetrieveRes =
+  useSeqvarsQueryPresetsPhenotypePrioRetrieveQuery({
+    presetsSetVersionUuid,
+    presetsPhenotypePrioUuid,
+  })
+/** Mutation for updating the phenotypePrio presets. */
+const phenotypePrioPresetsUpdate =
+  useSeqvarsQueryPresetsPhenotypePrioUpdateMutation()
 
 /** Shortcut to the number of phenotypePrio presets, used for rank. */
-const maxRank = computed<number>(() => {
-  if (props.presetSetVersion === undefined) {
-    return 0
-  }
-  const presetSetVersion = seqvarsPresetsStore.presetSetVersions.get(
-    props.presetSetVersion,
-  )
-  if (!presetSetVersion) {
-    return 0
-  }
-  return presetSetVersion.seqvarsquerypresetsphenotypeprio_set.length
-})
+const maxRank = computed<number>(
+  () =>
+    presetsSetVersionRetrieveRes.data.value
+      ?.seqvarsquerypresetsphenotypeprio_set.length ?? 0,
+)
 
 /** Rules for data validation. */
 const rules = {
@@ -57,162 +83,140 @@ const rules = {
 /** Ref to the form. */
 const formRef = ref<VForm | undefined>(undefined)
 
-/** Fill the data from the store. */
-const fillData = () => {
-  // Guard against missing preset set version or phenotypePrio.
-  if (
-    props.presetSetVersion === undefined ||
-    props.phenotypePrioPresets === undefined
-  ) {
-    return
-  }
-  // Attempt to obtain the model from the store.
-  const presetSetVersion = seqvarsPresetsStore.presetSetVersions.get(
-    props.presetSetVersion,
-  )
-  if (!presetSetVersion) {
-    emit('message', {
-      text: 'Failed to find preset set version.',
-      color: 'error',
-    })
-    return
-  }
-  const phenotypePrioPresets =
-    presetSetVersion?.seqvarsquerypresetsphenotypeprio_set.find(
-      (elem) => elem.sodar_uuid === props.phenotypePrioPresets,
-    )
-  if (!phenotypePrioPresets) {
-    emit('message', {
-      text: 'Failed to find phenotypePrio presets.',
-      color: 'error',
-    })
-    return
-  }
-
-  data.value = { ...phenotypePrioPresets }
-}
-
-/**
- * Update the phenotypePrio presets in the store.
- *
- * Used directly when changing the rank (to minimize UI delay).
- *
- * @param rankDelta The delta to apply to the rank, if any.
- */
-const updatePhenotypePrioPresets = async (rankDelta: number = 0) => {
-  // Guard against missing/readonly/non-draft preset set version or missing phenotypePrio.
-  if (
-    props.phenotypePrioPresets === undefined ||
-    props.presetSetVersion === undefined ||
-    seqvarsPresetsStore.presetSetVersions.get(props.presetSetVersion)
-      ?.status !== PresetSetVersionState.DRAFT ||
-    data.value === undefined
-  ) {
-    return
-  }
-
-  // If necessary, update the rank of the other item as well via API and set
-  // the new rank to `data.value.rank`.
-  if (rankDelta !== 0) {
-    const version = seqvarsPresetsStore.presetSetVersions.get(
-      props.presetSetVersion,
-    )
-    if (
-      version === undefined ||
-      data.value.rank === undefined ||
-      data.value.rank + rankDelta < 1 ||
-      data.value.rank + rankDelta > maxRank.value
-    ) {
-      // Guard against invalid rank and version.
-      return
-    }
-    // Find the next smaller or larger item, sort by rank.
-    const others = version.seqvarsquerypresetsphenotypeprio_set.filter(
-      (elem) => {
-        if (elem.sodar_uuid === props.phenotypePrioPresets) {
-          return false
-        }
-        if (rankDelta < 0) {
-          return (elem.rank ?? 0) < (data.value?.rank ?? 0)
-        } else {
-          return (elem.rank ?? 0) > (data.value?.rank ?? 0)
-        }
-      },
-    )
-    others.sort((a, b) => (a.rank ?? 0) - (b.rank ?? 0))
-    // Then, pick the other item to flip ranks with.
-    const other = others[rankDelta < 0 ? others.length - 1 : 0]
-    // Store the other's rank in `data.value.rank` and update other via API.
-    if (other) {
-      const dataRank = data.value.rank
-      data.value.rank = other.rank
-      other.rank = dataRank
-      try {
-        await seqvarsPresetsStore.updateQueryPresetsPhenotypePrio(
-          props.presetSetVersion,
-          other,
-        )
-      } catch (error) {
-        emit('message', {
-          text: 'Failed to update phenotypePrio presets rank.',
-          color: 'error',
-        })
-      }
-    }
-  }
-
+/** Helper to apply a patch to the current `presetsPhenotypePrioRetrieveRes.data.value`. */
+const applyMutation = async (
+  patch: Partial<SeqvarsQueryPresetsPhenotypePrio>,
+  rankDelta: number = 0,
+) => {
   // Guard against invalid form data.
   const validateResult = await formRef.value?.validate()
   if (validateResult?.valid !== true) {
     return
   }
+  // Short-circuit if patch is undefined or presets version undefine dor not in draft state.
+  if (
+    props.presetSet === undefined ||
+    props.presetSetVersion === undefined ||
+    presetsPhenotypePrioRetrieveRes.data.value === undefined ||
+    presetsSetVersionRetrieveRes.data.value === undefined ||
+    presetsSetVersionRetrieveRes.data.value.status !==
+      PresetSetVersionState.DRAFT
+  ) {
+    return
+  } else {
+    patch.rank = presetsPhenotypePrioRetrieveRes.data.value.rank
+  }
+
+  // Helper to update the rank of the other item as well.
+  const updateOtherItemRank = async () => {
+    if (props.presetSetVersion !== undefined && rankDelta !== 0) {
+      const version = presetsSetVersionRetrieveRes.data.value
+      if (
+        version === undefined ||
+        patch.rank === undefined ||
+        patch.rank + rankDelta < 1 ||
+        patch.rank + rankDelta > maxRank.value
+      ) {
+        // Guard against invalid or missing data.
+        return
+      }
+      // Find the next smaller or larger item, sort by rank.
+      const others = version.seqvarsquerypresetsphenotypeprio_set.filter(
+        (elem) => {
+          if (elem.sodar_uuid === props.phenotypePrioPresets) {
+            return false
+          }
+          if (rankDelta < 0) {
+            return (elem.rank ?? 0) < (patch?.rank ?? 0)
+          } else {
+            return (elem.rank ?? 0) > (patch?.rank ?? 0)
+          }
+        },
+      )
+      others.sort((a, b) => (a.rank ?? 0) - (b.rank ?? 0))
+      // Then, pick the other item to flip ranks with.
+      const other = { ...others[rankDelta < 0 ? others.length - 1 : 0] }
+      // Store the other's rank in `data.rank` and update other via API.
+      if (!!other) {
+        const newOtherRank = patch.rank
+        patch.rank = other.rank
+        other.rank = newOtherRank
+        try {
+          await phenotypePrioPresetsUpdate.mutateAsync({
+            path: {
+              querypresetssetversion: props.presetSetVersion,
+              querypresetsphenotypeprio: other.sodar_uuid,
+            },
+            body: {
+              ...other,
+              rank: newOtherRank,
+            },
+          })
+        } catch (error) {
+          emit('message', {
+            text: `Failed to update other phenotypePrio presets rank: ${error}`,
+            color: 'error',
+          })
+        }
+      }
+    }
+  }
+
+  // First, apply rank update to other data object and to patch if applicable.
+  // On success, the patch to the data object.
   try {
-    await seqvarsPresetsStore.updateQueryPresetsPhenotypePrio(
-      props.presetSetVersion,
-      data.value,
-    )
+    await updateOtherItemRank()
+    await phenotypePrioPresetsUpdate.mutateAsync({
+      path: {
+        querypresetssetversion: props.presetSetVersion,
+        querypresetsphenotypeprio:
+          presetsPhenotypePrioRetrieveRes.data.value.sodar_uuid,
+      },
+      body: {
+        ...presetsPhenotypePrioRetrieveRes.data.value,
+        ...patch,
+      },
+    })
+    // Explicitely invalidate the query presets set version as the title and rank
+    // can change and the version stores the category presets as well.
+    invalidateSeqvarQueryPresetsSetVersionKeys(queryClient, {
+      querypresetsset: props.presetSet,
+      querypresetssetversion: props.presetSetVersion,
+    })
   } catch (error) {
     emit('message', {
-      text: 'Failed to update phenotypePrio presets.',
+      text: `Failed to update phenotypePrio presets: ${error}`,
       color: 'error',
     })
   }
 }
-
-/**
- * Update the phenotypePrio presets in the store -- debounced.
- *
- * Used for updating non-rank fields so that the UI does not lag.
- */
-const updatePhenotypePrioPresetsDebounced = debounce(
-  updatePhenotypePrioPresets,
-  CATEGORY_PRESETS_DEBOUNCE_WAIT,
-  { leading: true, trailing: true },
-)
-
-// Load model data from store when the UUID changes.
-watch(
-  () => props.phenotypePrioPresets,
-  () => fillData(),
-)
-// Also, load model data from store when mounted.
-onMounted(() => fillData())
-
-// Watch the data and trigger a store update.
-watch(data, () => updatePhenotypePrioPresetsDebounced(), { deep: true })
 </script>
 
 <template>
-  <h4>Phenotype Prio Presets &raquo;{{ data?.label ?? 'UNDEFINED' }}&laquo;</h4>
+  <h4>
+    Phenotype Prio Presets &raquo;{{
+      presetsPhenotypePrioRetrieveRes.data.value?.label ?? 'UNDEFINED'
+    }}&laquo;
+  </h4>
 
-  <v-skeleton-loader v-if="!data" type="article" />
+  <v-skeleton-loader
+    v-if="presetsPhenotypePrioRetrieveRes.status.value !== 'success'"
+    type="article"
+  />
+
   <v-form v-else ref="formRef">
     <v-text-field
-      v-model="data.label"
+      :model-value="presetsPhenotypePrioRetrieveRes.data.value?.label"
       :rules="[rules.required]"
       label="Label"
       clearable
       :disabled="readonly"
+      @update:model-value="
+        (label) =>
+          applyMutation({
+            label,
+          })
+      "
     />
 
     <div>
@@ -220,18 +224,22 @@ watch(data, () => updatePhenotypePrioPresetsDebounced(), { deep: true })
         <v-btn
           prepend-icon="mdi-arrow-up-circle-outline"
           :disabled="
-            props.readonly || data.rank === undefined || data.rank <= 1
+            props.readonly ||
+            presetsPhenotypePrioRetrieveRes.data.value?.rank === undefined ||
+            presetsPhenotypePrioRetrieveRes.data.value?.rank <= 1
           "
-          @click="updatePhenotypePrioPresets(-1)"
+          @click="applyMutation({}, -1)"
         >
           Move Up
         </v-btn>
         <v-btn
           prepend-icon="mdi-arrow-down-circle-outline"
           :disabled="
-            props.readonly || data.rank === undefined || data.rank >= maxRank
+            props.readonly ||
+            presetsPhenotypePrioRetrieveRes.data.value?.rank === undefined ||
+            presetsPhenotypePrioRetrieveRes.data.value?.rank >= maxRank
           "
-          @click="updatePhenotypePrioPresets(1)"
+          @click="applyMutation({}, 1)"
         >
           Move Down
         </v-btn>
@@ -241,21 +249,36 @@ watch(data, () => updatePhenotypePrioPresetsDebounced(), { deep: true })
     <v-sheet class="text-center font-italic bg-grey-lighten-3 pa-3 mt-3">
       Phenotype priorization presets editor is not implemented yet.
     </v-sheet>
+
     <div>
       Enabled:
-      {{ data.phenotype_prio_enabled ? 'Yes' : 'No' }}
+      {{
+        presetsPhenotypePrioRetrieveRes.data.value?.phenotype_prio_enabled
+          ? 'Yes'
+          : 'No'
+      }}
     </div>
     <div>
       Algorithm:
-      {{ data.phenotype_prio_algorithm ?? 'N/A' }}
+      {{
+        presetsPhenotypePrioRetrieveRes.data.value?.phenotype_prio_algorithm ??
+        'N/A'
+      }}
     </div>
     <div>
       Terms:
-      <span v-if="data?.terms?.length === 0" class="text-grey-darken-2">
+      <span
+        v-if="presetsPhenotypePrioRetrieveRes.data.value?.terms?.length === 0"
+        class="text-grey-darken-2"
+      >
         N/A
       </span>
       <span v-else>
-        <v-chip v-for="(term, index) in data.terms ?? []" :key="index">
+        <v-chip
+          v-for="(term, index) in presetsPhenotypePrioRetrieveRes.data.value
+            ?.terms ?? []"
+          :key="index"
+        >
           {{ term }}
         </v-chip>
       </span>
--- frontend/src/seqvars/components/PresetsEditor/QueryPresetsEditor.vue
+++ frontend/src/seqvars/components/PresetsEditor/QueryPresetsEditor.vue
@@ -638,7 +638,7 @@ watch(
               @message="(event) => emit('message', event)"
             />
           </div>
-          <!--<div v-else-if="selectedCategory === PresetsCategory.PHENOTYPE_PRIO">
+          <div v-else-if="selectedCategory === PresetsCategory.PHENOTYPE_PRIO">
             <CategoryPresetsPhenotypePrioEditor
               :preset-set="presetSet"
               :preset-set-version="presetSetVersion"
@@ -660,7 +660,7 @@ watch(
               @message="(event) => emit('message', event)"
             />
           </div>
-          <div v-else-if="selectedCategory === PresetsCategory.CLINVAR">
+          <!--<div v-else-if="selectedCategory === PresetsCategory.CLINVAR">
             <CategoryPresetsClinvarEditor
               :preset-set="presetSet"
               :preset-set-version="presetSetVersion"
