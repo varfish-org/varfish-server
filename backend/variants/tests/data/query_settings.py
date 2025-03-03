32ce89c707350b529c52ef1b834907b54a743c97
Oliver Stolpe <oliver.stolpe@bih-charite.de>
2025-03-03 11:58:16 +0100

    Removed unneccessary comments

--- backend/variants/tests/data/query_settings.py
+++ backend/variants/tests/data/query_settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d12e614f2cca59cea8a897ca695764dd372dbf270d4ad527581f871bdd0b05e6
-size 10258
+oid sha256:13227f66bcbed05de2b9dbfabf798657891066cb763ccef83e6fcb3da7395726
+size 10233
