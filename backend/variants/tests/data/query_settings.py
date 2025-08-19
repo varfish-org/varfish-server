7b7369ad2ca5246c40b8beabb519113a2213a7ec
Oliver Stolpe <oliver.stolpe@charite.de>
2025-08-19 08:59:42 +0200

    fix: clinvar paranoid checkbox has no effect (#2392)

--- backend/variants/tests/data/query_settings.py
+++ backend/variants/tests/data/query_settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f877e0b6e42ab0ab936faf87e46e282fcfea08b3aec8f0352dc9cf13b2644611
-size 10317
+oid sha256:0519fe49564f2c03ff3e9ed75d6025d2a97242699a818ac709ca6be16ad277c5
+size 10353
